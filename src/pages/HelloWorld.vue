f20417212ed7c06bb8e016881582817e1f31c470
retrun18 <648437254@qq.com>
2017-11-23 13:44:53 +0800

    update

---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wraper">this is test home page</div>
+</template>
+<script scoped>
+  export default{
+      data(){
+          return{
+              name:"test name",
+          }
+      },
+    mounted(){
+          alert(this.name)
+    }
+  }
+</script>
+<style lang="scss">
+  .wraper{
+    background-color:red;
+  }
+  body{
+    margin: 0;
+  }
+</style>
